f5a1c2b5dd2a3e71db0038adfd66bc8152f06b21
Renoir Lucena <renoirlucena@gmail.com>
2023-09-14 23:43:51 -0500

    UNDO Rails.application.configure do to FirstApp::Application.configure do

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 require "active_support/core_ext/integer/time"
 
-FirstApp::Application.configure do
+Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.secret_key_base = Rails.application.credentials.secret_key_base
 

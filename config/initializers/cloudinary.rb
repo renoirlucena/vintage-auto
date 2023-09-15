fd0eed01a3a7bd5f3c8ca5beffca5e62a0cc22da
Renoir Lucena <renoirlucena@gmail.com>
2023-09-14 21:59:07 -0500

    Configure Cloudinary

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,5 +1,7 @@
 Cloudinary.config do |config|
-  config.cloud_name = ENV['dvhbdqwsv']
-  config.api_key = ENV['683945892518373']
-  config.api_secret = ENV['LmLFQ3VKoCvggR4ntqfAV3SHfSI']
+  config.cloud_name = 'dvhbdqwsv'
+  config.api_key = '683945892518373'
+  config.api_secret = 'LmLFQ3VKoCvggR4ntqfAV3SHfSI'
+  config.secure = true
+  config.cdn_subdomain = true
 end

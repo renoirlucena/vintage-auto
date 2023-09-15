c5871d040e67615a8a2c73340b695e12fc63fbf9
Renoir Lucena <renoirlucena@gmail.com>
2023-09-14 19:48:44 -0500

    Update production database configuration for Heroku

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module Rbnb
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.require_master_key = true
+
 
     # Configuration for the application, engines, and railties goes here.
     #
